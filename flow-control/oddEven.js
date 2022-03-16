87c49b3885c02bcac7c3afb06e7d5bc9c78fd190
Kiril-Pavlov <Kire_P@yahoo.com>
2022-03-16 12:41:32 +0100

    Create oddEven.js
    Code that return if a number is odd or even

---
+++ flow-control/oddEven.js
@@ -0,0 +1,9 @@
+// take input from the user
+let number = prompt("Enter a number: ");
+
+//check if the number odd or even by dividing with 2
+if (number % 2 == 0) {
+  console.log(`The number ${number} is even.`);
+}else {
+  console.log(`The number ${number} is odd.`);
+}
